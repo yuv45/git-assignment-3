ea5500715ef35f431ae71f5bbe9e515431793658
Sushant Arora CA <aroravinay2004@gmail.com>
2023-08-25 17:45:15 +0530

    check prime no. file is added

---
+++ prime.js
@@ -0,0 +1,14 @@
+function check_prime(number){
+  let count =0;
+  for(let i=0;i<=number;i++){
+    if(number%i==0){
+      count+2
+    }
+    else{
+      nul
+    }
+
+  }
+    (count==0)?console.log("The no.is prime"):console.log("The no.is prime")
+}
+check_prime(13}				
